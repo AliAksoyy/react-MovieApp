5e5c2a88d59f12415c62c2de0f17e4849874f9cf
AliAksoyy <alifeyza4017@gmail.com>
2022-11-27 23:34:07 +0300

    added

--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -2,7 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  
+  apiKey: "AIzaSyAfV8sBxYNRzRv22LVD4fj7v2RXpX-L8js",
+  authDomain: "react-movieapp-160bf.firebaseapp.com",
+  projectId: "react-movieapp-160bf",
+  storageBucket: "react-movieapp-160bf.appspot.com",
+  messagingSenderId: "238671298823",
+  appId: "1:238671298823:web:38d81fe3742e9b8e1c05a2"
 };
 
 
